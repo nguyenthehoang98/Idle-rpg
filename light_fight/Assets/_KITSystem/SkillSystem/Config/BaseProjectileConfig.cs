4d5309376f7442a526ba58707d2b9feeba9b66e2
hoangpc <wolfbytegame@gmail.com>
2026-05-05 07:53:23 +0700

    edit

--- light_fight/Assets/_KITSystem/SkillSystem/Config/BaseProjectileConfig.cs
+++ light_fight/Assets/_KITSystem/SkillSystem/Config/BaseProjectileConfig.cs
@@ -16,12 +16,13 @@ namespace _KITSystem.SkillSystem.Config
         [HideIf("damageTickerType", DamageTickerType.None), Indent]
         public float damageTickerIntervalInSeconds = 1f;
 
-        [TitleGroup("Projectile : Collision"), Indent]
+        [TitleGroup("Projectile : Collision"), Indent, ShowIf("Type", ProjectileType.Ranger)]
         [Tooltip("Giới hạn va chạm của viên đạn, nếu đủ số lần thì đạn sẽ tự hủy")]
         public int maximumCollision = 1;
 
         [Indent, Tooltip(
             "Ngưỡng thời gian viên đạn có thể va chạm với 1 Object lần nữa. \n(Ví dụ bãi độc gây sát thương mỗi 0.3s nếu đứng trên nó)")]
+        [ShowIf("Type", ProjectileType.Ranger)]
         public float collisionResetIntervalInSeconds = 1 / 30f;
 
         public abstract ProjectileType Type { get; }
