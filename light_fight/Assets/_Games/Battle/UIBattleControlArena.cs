7814bca0c328f01d227dd2a3dbae3c1a6cc8995b
hoangpc <wolfbytegame@gmail.com>
2026-05-10 10:34:22 +0700

    update ui

--- light_fight/Assets/_Games/Battle/UIBattleControlArena.cs
+++ light_fight/Assets/_Games/Battle/UIBattleControlArena.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Sirenix.OdinInspector;
 using TMPro;
@@ -16,8 +15,11 @@ namespace _Games.Battle
         [SerializeField] private Color upColor = new Color(0, 1, 0);
         [SerializeField] private Color centerColor = new Color(1, 1, 1);
         [SerializeField] private Color downColor = new Color(1, 0, 0);
+        [SerializeField, Range(1f, 5f)] private float speed = 2f;
         [SerializeField] private Slider upSlider;
+        [SerializeField] private UICornersGradient upFill2;
         [SerializeField] private Slider downSlider;
+        [SerializeField] private UICornersGradient downFill2;
         [SerializeField] private AnimationCurve upProgressCurve;
         [SerializeField] private AnimationCurve downProgressCurve;
 
@@ -40,6 +42,20 @@ namespace _Games.Battle
 
         private void Awake()
         {
+            downSlider.fillRect.GetComponent<Image>().color = downColor;
+            downFill2.m_topRightColor = new Color(downColor.r, downColor.g, downColor.b, 1f);
+            downFill2.m_bottomRightColor = new Color(downColor.r, downColor.g, downColor.b, 1f);
+            downFill2.m_topLeftColor = new Color(downColor.r, downColor.g, downColor.b, 0.1f);
+            downFill2.m_bottomLeftColor = new Color(downColor.r, downColor.g, downColor.b, 0.1f);
+            downFill2.gameObject.SetActive(false);
+            
+            upSlider.fillRect.GetComponent<Image>().color = upColor;
+            upFill2.m_topLeftColor = new Color(upColor.r, upColor.g, upColor.b, 1f);
+            upFill2.m_bottomLeftColor = new Color(upColor.r, upColor.g, upColor.b, 1f);
+            upFill2.m_topRightColor = new Color(upColor.r, upColor.g, upColor.b, 0.1f);
+            upFill2.m_bottomRightColor = new Color(upColor.r, upColor.g, upColor.b, 0.1f);
+            upFill2.gameObject.SetActive(false);
+            
             StartCoroutine(BuildLayout());
         }
 
@@ -48,7 +64,7 @@ namespace _Games.Battle
             if (isPressing)
             {
                 bool shouldUpdate = elapsedTime < 1;
-                elapsedTime += Time.deltaTime;
+                elapsedTime += Time.deltaTime * speed;
                 elapsedTime = Mathf.Clamp01(elapsedTime);
                 if (toLeft)
                 {
@@ -72,8 +88,9 @@ namespace _Games.Battle
             else
             {
                 bool shouldUpdate = elapsedTime > 0;
-                elapsedTime -= Time.deltaTime;
+                elapsedTime -= Time.deltaTime * speed;
                 elapsedTime = Mathf.Clamp01(elapsedTime);
+                bool shouldInactiveGameObject = shouldUpdate && elapsedTime == 0;
                 if (toLeft)
                 {
                     fill = (int)(downProgressCurve.Evaluate(elapsedTime) * MIN);
@@ -92,6 +109,14 @@ namespace _Games.Battle
 
                     UpdateProgress();
                 }
+
+                if (shouldInactiveGameObject)
+                {
+                    if (toLeft) buttonRight.interactable = true;
+                    else buttonLeft.interactable = true;
+                    upFill2.gameObject.SetActive(false);
+                    downFill2.gameObject.SetActive(false);
+                }
             }
         }
 
@@ -100,11 +125,11 @@ namespace _Games.Battle
             buttonRight.interactable = false;
             toLeft = true;
             isPressing = true;
+            downFill2.gameObject.SetActive(true);
         }
 
         public void OnLeftPointerUp()
         {
-            buttonRight.interactable = true;
             isPressing = false;
         }
 
@@ -113,16 +138,19 @@ namespace _Games.Battle
             buttonLeft.interactable = false;
             toLeft = false;
             isPressing = true;
+            upFill2.gameObject.SetActive(true);
         }
 
         public void OnRightPointerUp()
         { 
-            buttonLeft.interactable = true;
             isPressing = false;
         }
 
         private void UpdateProgress()
         {
+            upFill2.transform.position = upSlider.handleRect.transform.position;
+            downFill2.transform.position = downSlider.handleRect.transform.position;
+            
             textProgress.text = fill + "%";
 
             Color color;
