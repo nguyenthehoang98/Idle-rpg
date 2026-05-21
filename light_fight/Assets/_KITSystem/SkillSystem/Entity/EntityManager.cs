f7293f53c591c21d28e9cb7549321de9bea4923a
hoangpc <wolfbytegame@gmail.com>
2026-05-21 21:52:37 +0700

    add entity & component system

---
+++ light_fight/Assets/_KITSystem/SkillSystem/Entity/EntityManager.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _KITSystem.SkillSystem.Entity
+{
+    public static class EntityManager
+    {
+        private static int[] entityVersions = new int[256];
+        private static readonly Stack<int> freeIds = new();
+        private static int nextId = 1;
+
+        static EntityManager()
+        {
+            Array.Fill(entityVersions, -1);
+        }
+
+        public static int ActiveCount { get; private set; }
+
+        #region Entity Lifecycle
+
+        public static int NewEntity()
+        {
+            int id = freeIds.Count > 0 ? freeIds.Pop() : AllocateId();
+            entityVersions[id] = 0;
+            ActiveCount++;
+            return id;
+        }
+
+        public static void DestroyEntity(int entity)
+        {
+            if (!IsAlive(entity))
+                return;
+
+            foreach (KeyValuePair<Type, IComponentPool> pool in ComponentRegistry.Pools)
+            {
+                pool.Value.Remove(entity);
+            }
+
+            entityVersions[entity] = -1;
+            freeIds.Push(entity);
+            ActiveCount--;
+        }
+
+        public static bool IsAlive(int entity)
+        {
+            return entity > 0 && entity < entityVersions.Length && entityVersions[entity] >= 0;
+        }
+
+        private static int AllocateId()
+        {
+            int id = nextId++;
+            if (id >= entityVersions.Length)
+                Array.Resize(ref entityVersions, id * 2);
+            return id;
+        }
+
+        #endregion
+
+        #region Component Operations
+
+        public static void AddComponent<T>(int entity, in T component) where T : unmanaged
+        {
+            if (!IsAlive(entity))
+            {
+#if DEBUG
+                throw new ArgumentException($"Entity {entity} is not alive.");       
+#endif
+                return;
+            }
+
+            if (ComponentManager<T>.Has(entity))
+            {
+#if DEBUG
+                throw new InvalidOperationException($"Entity {entity} already has {typeof(T).Name}.");       
+#endif
+                return;
+            }
+
+            ComponentManager<T>.Add(entity, component);
+            ComponentRegistry.Register(typeof(T), ComponentPoolDelegates<T>.Instance);
+        }
+
+        public static ref T GetComponent<T>(int entity) where T : unmanaged
+        {
+            return ref ComponentManager<T>.Get(entity);
+        }
+
+        public static bool TryGetComponent<T>(int entity, out T component) where T : unmanaged
+        {
+            return ComponentManager<T>.TryGet(entity, out component);
+        }
+
+        public static bool HasComponent<T>(int entity) where T : unmanaged
+        {
+            return ComponentManager<T>.Has(entity);
+        }
+
+        public static void RemoveComponent<T>(int entity) where T : unmanaged
+        {
+            ComponentManager<T>.Remove(entity);
+        }
+
+        #endregion
+
+        #region Query
+
+        public static IEnumerable<int> Query<T>() where T : unmanaged
+        {
+            foreach (var id in ComponentManager<T>.EntityIds)
+                yield return id;
+        }
+
+        public static IEnumerable<int> Query<T1, T2>()
+            where T1 : unmanaged
+            where T2 : unmanaged
+        {
+            foreach (var id in ComponentManager<T1>.EntityIds)
+            {
+                if (ComponentManager<T2>.Has(id))
+                    yield return id;
+            }
+        }
+
+        public static IEnumerable<int> Query<T1, T2, T3>()
+            where T1 : unmanaged
+            where T2 : unmanaged
+            where T3 : unmanaged
+        {
+            foreach (var id in ComponentManager<T1>.EntityIds)
+            {
+                if (ComponentManager<T2>.Has(id) && ComponentManager<T3>.Has(id))
+                    yield return id;
+            }
+        }
+
+        #endregion
+
+        #region Cleanup
+
+        public static void Clear()
+        {
+            foreach (KeyValuePair<Type, IComponentPool> pool in ComponentRegistry.Pools)
+            {
+                pool.Value.Clear();
+            }
+
+            Array.Fill(entityVersions, -1);
+            freeIds.Clear();
+            nextId = 1;
+            ActiveCount = 0;
+        }
+
+        #endregion
+    }
+
+    internal static class ComponentPoolDelegates<T> where T : unmanaged
+    {
+        public static readonly IComponentPool Instance = new ComponentPoolImpl<T>();
+    }
+
+    internal interface IComponentPool
+    {
+        void Remove(int id);
+        void Clear();
+    }
+
+    internal sealed class ComponentPoolImpl<T> : IComponentPool where T : unmanaged
+    {
+        public void Remove(int id) => ComponentManager<T>.Remove(id);
+        public void Clear() => ComponentManager<T>.Clear();
+    }
+
+    internal static class ComponentRegistry
+    {
+        private static readonly Dictionary<Type, IComponentPool> pools = new();
+
+        public static IReadOnlyDictionary<Type, IComponentPool> Pools => pools;
+
+        public static void Register(Type type, IComponentPool pool)
+        {
+            if (!pools.ContainsKey(type))
+                pools[type] = pool;
+        }
+    }
+}
---
+++ light_fight/Assets/_KITSystem/SkillSystem/Unitest/EntityManagerTesting.cs
@@ -0,0 +1,423 @@
+using System.Collections.Generic;
+using _KITSystem.SkillSystem.Entity;
+using NUnit.Framework;
+
+namespace _KITSystem.SkillSystem.Unitest
+{
+    public class EntityManagerTesting
+    {
+        [SetUp]
+        public void Setup()
+        {
+            EntityManager.Clear();
+        }
+
+        #region Entity Lifecycle
+
+        [Test]
+        public void NewEntity_ShouldReturnValidId()
+        {
+            int entity = EntityManager.NewEntity();
+
+            Assert.IsTrue(entity > 0);
+            Assert.IsTrue(EntityManager.IsAlive(entity));
+            Assert.AreEqual(1, EntityManager.ActiveCount);
+        }
+
+        [Test]
+        public void NewEntity_ShouldIncrementId()
+        {
+            int e1 = EntityManager.NewEntity();
+            int e2 = EntityManager.NewEntity();
+            int e3 = EntityManager.NewEntity();
+
+            Assert.AreNotEqual(e1, e2);
+            Assert.AreNotEqual(e2, e3);
+            Assert.AreEqual(3, EntityManager.ActiveCount);
+        }
+
+        [Test]
+        public void DestroyEntity_ShouldMarkDead()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.DestroyEntity(entity);
+
+            Assert.IsFalse(EntityManager.IsAlive(entity));
+            Assert.AreEqual(0, EntityManager.ActiveCount);
+        }
+
+        [Test]
+        public void DestroyEntity_ShouldFreeIdForReuse()
+        {
+            int e1 = EntityManager.NewEntity();
+            EntityManager.DestroyEntity(e1);
+
+            int e2 = EntityManager.NewEntity();
+
+            Assert.AreEqual(e1, e2);
+            Assert.AreEqual(1, EntityManager.ActiveCount);
+        }
+
+        [Test]
+        public void DestroyEntity_Invalid_ShouldNotCrash()
+        {
+            Assert.DoesNotThrow(() => EntityManager.DestroyEntity(0));
+            Assert.DoesNotThrow(() => EntityManager.DestroyEntity(99999));
+        }
+
+        [Test]
+        public void DestroyEntity_DoubleDestroy_ShouldNotCrash()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.DestroyEntity(entity);
+            EntityManager.DestroyEntity(entity);
+
+            Assert.AreEqual(0, EntityManager.ActiveCount);
+        }
+
+        [Test]
+        public void Clear_ShouldResetAll()
+        {
+            EntityManager.NewEntity();
+            EntityManager.NewEntity();
+            EntityManager.NewEntity();
+
+            EntityManager.Clear();
+
+            Assert.AreEqual(0, EntityManager.ActiveCount);
+            Assert.IsFalse(EntityManager.IsAlive(1));
+        }
+
+        [Test]
+        public void StressTest_EntityCreateDestroy()
+        {
+            const int ITER = 10000;
+
+            for (int i = 0; i < ITER; i++)
+            {
+                int entity = EntityManager.NewEntity();
+                Assert.IsTrue(EntityManager.IsAlive(entity));
+                EntityManager.DestroyEntity(entity);
+                Assert.IsFalse(EntityManager.IsAlive(entity));
+            }
+
+            Assert.AreEqual(0, EntityManager.ActiveCount);
+        }
+
+        #endregion
+
+        #region Component Operations
+
+        [Test]
+        public void AddComponent_ShouldBeRetrievable()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent { Current = 100, Max = 100 });
+
+            Assert.IsTrue(EntityManager.HasComponent<HealthComponent>(entity));
+
+            ref var health = ref EntityManager.GetComponent<HealthComponent>(entity);
+            Assert.AreEqual(100, health.Current);
+        }
+
+        [Test]
+        public void AddComponent_ShouldModifyByRef()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent { Current = 100, Max = 100 });
+
+            ref var health = ref EntityManager.GetComponent<HealthComponent>(entity);
+            health.Current = 50;
+
+            ref var health2 = ref EntityManager.GetComponent<HealthComponent>(entity);
+            Assert.AreEqual(50, health2.Current);
+        }
+
+        [Test]
+        public void TryGetComponent_ShouldReturnFalse_WhenMissing()
+        {
+            int entity = EntityManager.NewEntity();
+
+            bool result = EntityManager.TryGetComponent<HealthComponent>(entity, out _);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void TryGetComponent_ShouldReturnTrue_WhenPresent()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent { Current = 50 });
+
+            bool result = EntityManager.TryGetComponent<HealthComponent>(entity, out var health);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(50, health.Current);
+        }
+
+        [Test]
+        public void RemoveComponent_ShouldRemove()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent());
+
+            EntityManager.RemoveComponent<HealthComponent>(entity);
+
+            Assert.IsFalse(EntityManager.HasComponent<HealthComponent>(entity));
+        }
+
+        [Test]
+        public void RemoveComponent_Missing_ShouldNotCrash()
+        {
+            int entity = EntityManager.NewEntity();
+
+            Assert.DoesNotThrow(() => EntityManager.RemoveComponent<HealthComponent>(entity));
+        }
+
+        [Test]
+        public void AddComponent_Duplicate_ShouldThrow()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent());
+
+            Assert.Throws<System.InvalidOperationException>(() =>
+                EntityManager.AddComponent(entity, new HealthComponent()));
+        }
+
+        [Test]
+        public void AddComponent_DeadEntity_ShouldThrow()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.DestroyEntity(entity);
+
+            Assert.Throws<System.ArgumentException>(() =>
+                EntityManager.AddComponent(entity, new HealthComponent()));
+        }
+
+        #endregion
+
+        #region Query
+
+        [Test]
+        public void Query_SingleComponent_ShouldReturnAllWithComponent()
+        {
+            int e1 = EntityManager.NewEntity();
+            int e2 = EntityManager.NewEntity();
+            int e3 = EntityManager.NewEntity();
+
+            EntityManager.AddComponent(e1, new HealthComponent { Current = 10 });
+            EntityManager.AddComponent(e2, new HealthComponent { Current = 20 });
+
+            var results = new List<int>();
+            foreach (var id in EntityManager.Query<HealthComponent>())
+                results.Add(id);
+
+            Assert.AreEqual(2, results.Count);
+            Assert.Contains(e1, results);
+            Assert.Contains(e2, results);
+            Assert.IsFalse(results.Contains(e3));
+        }
+
+        [Test]
+        public void Query_TwoComponents_ShouldReturnIntersection()
+        {
+            int e1 = EntityManager.NewEntity();
+            int e2 = EntityManager.NewEntity();
+            int e3 = EntityManager.NewEntity();
+
+            EntityManager.AddComponent(e1, new HealthComponent());
+            EntityManager.AddComponent(e1, new PositionComponent());
+
+            EntityManager.AddComponent(e2, new HealthComponent());
+
+            EntityManager.AddComponent(e3, new PositionComponent());
+
+            var results = new List<int>();
+            foreach (var id in EntityManager.Query<HealthComponent, PositionComponent>())
+                results.Add(id);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.Contains(e1, results);
+        }
+
+        [Test]
+        public void Query_ThreeComponents_ShouldReturnIntersection()
+        {
+            int e1 = EntityManager.NewEntity();
+            int e2 = EntityManager.NewEntity();
+
+            EntityManager.AddComponent(e1, new HealthComponent());
+            EntityManager.AddComponent(e1, new PositionComponent());
+            EntityManager.AddComponent(e1, new VelocityComponent());
+
+            EntityManager.AddComponent(e2, new HealthComponent());
+            EntityManager.AddComponent(e2, new PositionComponent());
+
+            var results = new List<int>();
+            foreach (var id in EntityManager.Query<HealthComponent, PositionComponent, VelocityComponent>())
+                results.Add(id);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.Contains(e1, results);
+            Assert.IsFalse(results.Contains(e2));
+        }
+
+        [Test]
+        public void Query_AfterDestroy_ShouldNotReturnDeadEntity()
+        {
+            int e1 = EntityManager.NewEntity();
+            int e2 = EntityManager.NewEntity();
+
+            EntityManager.AddComponent(e1, new HealthComponent());
+            EntityManager.AddComponent(e2, new HealthComponent());
+
+            EntityManager.DestroyEntity(e1);
+
+            var results = new List<int>();
+            foreach (var id in EntityManager.Query<HealthComponent>())
+                results.Add(id);
+
+            Assert.AreEqual(1, results.Count);
+            Assert.Contains(e2, results);
+        }
+
+        [Test]
+        public void Query_AfterRemoveComponent_ShouldNotReturnEntity()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent());
+
+            EntityManager.RemoveComponent<HealthComponent>(entity);
+
+            var results = new List<int>();
+            foreach (var id in EntityManager.Query<HealthComponent>())
+                results.Add(id);
+
+            Assert.AreEqual(0, results.Count);
+        }
+
+        #endregion
+
+        #region DestroyEntity Cleans Components
+
+        [Test]
+        public void DestroyEntity_ShouldRemoveAllComponents()
+        {
+            int entity = EntityManager.NewEntity();
+            EntityManager.AddComponent(entity, new HealthComponent());
+            EntityManager.AddComponent(entity, new PositionComponent());
+            EntityManager.AddComponent(entity, new VelocityComponent());
+
+            EntityManager.DestroyEntity(entity);
+
+            Assert.IsFalse(EntityManager.HasComponent<HealthComponent>(entity));
+            Assert.IsFalse(EntityManager.HasComponent<PositionComponent>(entity));
+            Assert.IsFalse(EntityManager.HasComponent<VelocityComponent>(entity));
+        }
+
+        [Test]
+        public void DestroyEntity_ThenReuseId_ShouldBeClean()
+        {
+            int e1 = EntityManager.NewEntity();
+            EntityManager.AddComponent(e1, new HealthComponent { Current = 999 });
+            EntityManager.DestroyEntity(e1);
+
+            int e2 = EntityManager.NewEntity();
+            Assert.AreEqual(e1, e2);
+
+            Assert.IsFalse(EntityManager.HasComponent<HealthComponent>(e2));
+        }
+
+        #endregion
+
+        #region Stress Tests
+
+        [Test]
+        public void StressTest_ComponentAddRemove()
+        {
+            const int ITER = 5000;
+            var entities = new int[100];
+
+            for (int i = 0; i < 100; i++)
+                entities[i] = EntityManager.NewEntity();
+
+            var rnd = new System.Random(42);
+
+            for (int i = 0; i < ITER; i++)
+            {
+                int idx = rnd.Next(100);
+                int entity = entities[idx];
+                int op = rnd.Next(4);
+                if (op == 0 && !EntityManager.HasComponent<HealthComponent>(entity))
+                    EntityManager.AddComponent(entity, new HealthComponent { Current = rnd.Next(100) });
+                else if (op == 1 && !EntityManager.HasComponent<PositionComponent>(entity))
+                    EntityManager.AddComponent(entity, new PositionComponent { X = rnd.Next(), Y = rnd.Next() });
+                else if (op == 2 && EntityManager.HasComponent<HealthComponent>(entity))
+                    EntityManager.RemoveComponent<HealthComponent>(entity);
+                else if (op == 3 && EntityManager.HasComponent<PositionComponent>(entity))
+                    EntityManager.RemoveComponent<PositionComponent>(entity);
+            }
+        }
+
+        [Test]
+        public void StressTest_MixedOperations()
+        {
+            const int ITER = 2000;
+            var active = new List<int>();
+            var rnd = new System.Random(123);
+
+            for (int i = 0; i < ITER; i++)
+            {
+                int op = rnd.Next(4);
+
+                if (op == 0)
+                {
+                    int entity = EntityManager.NewEntity();
+                    EntityManager.AddComponent(entity, new HealthComponent { Current = rnd.Next(100) });
+                    EntityManager.AddComponent(entity, new PositionComponent());
+                    active.Add(entity);
+                }
+                else if (op == 1 && active.Count > 0)
+                {
+                    int idx = rnd.Next(active.Count);
+                    EntityManager.DestroyEntity(active[idx]);
+                    active.RemoveAt(idx);
+                }
+                else if (op == 2 && active.Count > 0)
+                {
+                    int idx = rnd.Next(active.Count);
+                    EntityManager.RemoveComponent<HealthComponent>(active[idx]);
+                }
+                else if (op == 3)
+                {
+                    foreach (var id in EntityManager.Query<HealthComponent, PositionComponent>())
+                    {
+                        ref var health = ref EntityManager.GetComponent<HealthComponent>(id);
+                        health.Current--;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    // Test components
+    internal struct HealthComponent
+    {
+        internal int Current;
+        internal int Max;
+    }
+
+    internal struct PositionComponent
+    {
+        internal float X;
+        internal float Y;
+    }
+
+    internal struct VelocityComponent
+    {
+        internal float VX;
+        internal float VY;
+    }
+}
