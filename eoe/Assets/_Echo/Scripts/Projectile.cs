2cc88e87c42a860021821ffaa9a072f7265ddd1b
hoangnt <nguyenthehoang98@gmail.com>
2026-06-11 23:53:09 +0700

    create fx

---
+++ eoe/Assets/_Echo/Scripts/Projectile.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+namespace _Echo.Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+        public float duration = 0.5f;
+        public float radius = 3;
+    
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                transform.position = Vector3.zero;
+                StartCoroutine(Lerp(Vector3.zero,
+                    new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * radius)
+                );
+            }
+        }
+
+        IEnumerator Lerp(Vector3 start, Vector3 end)
+        {
+            transform.position = start;
+     
+            Vector2 direction = end - start;
+        
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            transform.eulerAngles = new Vector3(0, 0, angle - 90);
+        
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+            
+                transform.position = Vector3.Lerp(start, end, Mathf.Clamp01(elapsed / duration));
+            
+                yield return null;
+            }
+        
+            transform.position = end;
+        }
+    }
+}
