882dc82da34a61a8dbb495d941b4a0301142d134
pc <pc>
2026-07-27 06:26:41 +0700

    stat simple

---
+++ tdsurvivor/Assets/_TDS/Statistics/StatGlobal.cs
@@ -0,0 +1,30 @@
+﻿namespace _TDS.Statistics
+{
+    /// <summary>
+    /// Hàm này để sử dụng trong code, phải dối chiếu với file config xem đúng Id ko tránh sai sót 
+    /// </summary>
+    public static class StatId
+    {
+        // Vital
+        public const int HP = 1001;
+        public const int HP_MAX = 1002;
+        public const int MP = 1003;
+        public const int MP_MAX = 1004;
+
+        // Combat
+        public const int ATK = 2001;
+        public const int DEF = 2002;
+        public const int MAGIC_ATK = 2003;
+        public const int MAGIC_DEF = 2004;
+        public const int CRIT_RATE = 2005;
+        public const int CRIT_DAMAGE = 2006;
+
+        // Movement
+        public const int MOVE_SPEED = 3001;
+        public const int ATTACK_SPEED = 3002;
+
+        // Economy
+        public const int GOLD_GAIN = 4001;
+        public const int EXP_GAIN = 4002;
+    }
+}
---
+++ tdsurvivor/Assets/_TDS/Statistics/StatRuntime.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using _GameToolkit.Statistics;
+
+namespace _TDS.Statistics
+{
+    public sealed class Stats
+    {
+        private Dictionary<int, Stat> stats = new Dictionary<int, Stat>();
+
+        public bool AddStat(int id, Stat stat)
+        {
+            return stats.TryAdd(id, stat);
+        }
+
+        public bool RemoveStat(int id)
+        {
+            return stats.Remove(id);
+        }
+
+        public bool TryGet(int id, out Stat stat)
+        {
+            return stats.TryGetValue(id, out stat);
+        }
+    }
+}
