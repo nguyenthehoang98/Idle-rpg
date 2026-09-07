b9d6a6643a70ec2592a4fbd09f608bada4f2fbbc
hoangnt <nguyenthehoang98@gmail.com>
2026-09-07 23:17:57 +0700

    test: cover ui preview page navigation

--- eoe/Assets/_TDS/Tests/PlayMode/UiPreviewTests.cs
+++ eoe/Assets/_TDS/Tests/PlayMode/UiPreviewTests.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-
 namespace _TDS.Tests.PlayMode
 {
     public sealed class UiPreviewTests
@@ -22,6 +21,31 @@ namespace _TDS.Tests.PlayMode
             Assert.That(canvas.Find("SafeArea/HeroCard"), Is.Not.Null);
             Assert.That(canvas.Find("SafeArea/Continue"), Is.Not.Null);
             Assert.That(canvas.Find("SafeArea/BottomNav"), Is.Not.Null);
+
+            Object.Destroy(root);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator PreviewBuildsEveryPage()
+        {
+            GameObject root = new GameObject("UiPreviewPagesTestRoot");
+            System.Type screenType = System.Type.GetType("_TDS.UI.UiPreviewScreen, Assembly-CSharp");
+            System.Type pageType = System.Type.GetType("_TDS.UI.UiPreviewPage, Assembly-CSharp");
+            Assert.That(screenType, Is.Not.Null);
+            Assert.That(pageType, Is.Not.Null);
+            Component screen = root.AddComponent(screenType);
+            System.Reflection.MethodInfo showPage = screenType.GetMethod("ShowPage");
+            Assert.That(showPage, Is.Not.Null);
+            yield return null;
+
+            foreach (string pageName in new[] { "Home", "Gameplay", "Reward", "Shop", "Augment", "Result" })
+            {
+                object page = System.Enum.Parse(pageType, pageName);
+                showPage.Invoke(screen, new[] { page });
+                yield return null;
+                Assert.That(root.transform.Find("UiCanvas/SafeArea"), Is.Not.Null, pageName);
+            }
 
             Object.Destroy(root);
             yield return null;
